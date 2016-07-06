50bdddc896992b456368e1b61283a09186cd5952
Márton Makrai <makrai.nlp@gmail.com>
2016-07-06 17:03:38 +0200

    this part may be OK for now

--- langid/laur/README.md
+++ langid/laur/README.md
@@ -1,12 +1,15 @@
-laur
+#  laur
+
+## about
 
 Configuration files and protocol directories for language recognition
 experiments with the [bob.bio.spear
 package](https://github.com/bioidiap/bob.bio.spear) on [IARPA
-Babel](https://www.iarpa.gov/index.php/research-programs/babel) Language Packs
+Babel](https://www.iarpa.gov/index.php/research-programs/babel) Language
+Packs.
 This software has been created for the [infra2 open speech
 archive](http://hlt.bme.hu/en/projects/speech) following the instructions in
 [this mail
 thread](https://groups.google.com/forum/#!topic/bob-devel/e4kNR-WCTQE).
-The name is laur is an abbreviation of _**la**ng**u**age **r**ecognition_
+The name _laur_ is an abbreviation of _**la**ng**u**age **r**ecognition_
 inspired by the way _spear_ abbreviates _**spea**ker **r**ecognition_.
--- langid/laur/babel.py
+++ langid/laur/babel.py
@@ -1,13 +1,16 @@
 import bob.db.verification.filelist
 import bob.bio.base
 
-custom_wav_directory = "[CUSTOM_WAV_DIRECTORY]"
+# babel_wav_directory = "[YOUR_HUNSPEECH_WAV_DIRECTORY]"
 
 database = bob.bio.base.database.DatabaseBob(
     database = bob.db.verification.filelist.Database(
-        'bob/bio/spear/config/database/custom/',
-        original_directory = custom_wav_directory,
+        # TODO Later replace this first argument by
+        # pkg_resources.resource_filename(
+        #     'bob.db.hunspeech', 'config/database/hunspeech')
+        '/mnt/store/hlt/Speech/LangPack/bob_list/', # Contains the file lists
+        original_directory = '/mnt/store/hlt/Speech/LangPack',
         original_extension = ".wav",),
-    name = "custom",
-    protocol = 'A',
+    name = "babel",
+    protocol = '', # There is only a single protocol
 )
