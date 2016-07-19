e0dddc41994d912a9b29311d8bb0b655614e0ec1
Pajkossy Katalin <pajkossy@ilab.sztaki.hu>
2016-07-19 11:22:03 +0200

    help messages and description for ArgumentParser

--- speaker_id/em-dia.py
+++ speaker_id/em-dia.py
@@ -25,9 +25,9 @@ class EmDiarizer:
         if sad_fn:
             self.sad_fn = sad_fn
         else:
-            self.steps.append(self.run_sad)
+            self.steps.append(self.run_segment)
             self.sad_fn = '{}/{}.sad'.format(output_dir, base)
-        self.steps.append(self.run_clustering)
+        self.steps.append(self.run_cluster)
         self.log_fn = '{}/{}.log'.format(output_dir, base)
         self.dia_fn = '{}/{}.dia'.format(output_dir, base)
 
@@ -36,13 +36,13 @@ class EmDiarizer:
             '--bits', '16', '--endian', 'little', self.raw_fn],
             stdout=self.log, stderr=self.log)
 
-    def run_sad(self):
+    def run_segment(self):
         src = '{}/release-2010-version-0-3/release/src/shout_segment'\
                 .format(self.src_dir)
         subprocess.check_call([src, '-a', self.raw_fn, '--am-segment',
             self.model, '-mo', self.sad_fn], stdout=self.log, stderr=self.log)
 
-    def run_clustering(self):
+    def run_cluster(self):
         src = '{}/release-2010-version-0-3/release/src/shout_cluster'\
                 .format(self.src_dir)
         subprocess.check_call([src, '-a', self.raw_fn, '-mo', self.dia_fn,
@@ -69,12 +69,22 @@ class EmDiarizer:
 
 def get_args():
 
-    p = ArgumentParser()
-    p.add_argument('input_fn')
-    p.add_argument('output_dir')
-    p.add_argument('shout_dir')
-    p.add_argument('-m', '--shout_model')
-    p.add_argument('-s', '--sad_fn')
+    p = ArgumentParser(description='Python wrapper for the speaker diarization module' +\
+            ' of the "SHoUT" speech recognition toolkit (by Marijn Huijbregts), with' +\
+            ' added audio conversion (using SoX -Sound eXchange- utility)')
+    p.add_argument('input_fn', help='input audio filename, can be of any' +\
+            ' audio format supported by sox (e.g. .wav, .mp3).' +\
+            ' If the extension is .raw, it is assumed to be a headerless' +\
+            ' raw audio file; mono, 16 kHz, 16 bits little endian.')
+    p.add_argument('output_dir', help='directory where to place all output files' +\
+            ' - the converted raw audio file (.raw), SHOUT outputs (.sad, .dia)' +\
+            ' and a logfile (.log)')
+    p.add_argument('shout_dir', help='directory containing SHOUT source and binaries' +\
+            ' (release-2010-version-0-3)')
+    p.add_argument('-m', '--shout_model', help='statistical model needed for segmenting;' +\
+            ' either this or the output of shout_segment is needed')
+    p.add_argument('-s', '--sad_fn', help='output of shout_segment; either this or a ' +\
+            'statistical model for running shout_segment is needed')
     args = p.parse_args()
     return args
 
