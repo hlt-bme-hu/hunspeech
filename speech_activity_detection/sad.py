05db56c424d06b64ccf5bc7e23cd2f56b7bea612
Judit Acs <judit@mokk.bme.hu>
2016-07-20 15:59:10 +0200

    untested version of SHOUT SAD wrapper

---
+++ speech_activity_detection/sad.py
@@ -0,0 +1,100 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Judit Acs <judit@sch.bme.hu>
+#
+# Distributed under terms of the MIT license.
+
+from argparse import ArgumentParser
+import os
+import subprocess
+
+
+class EMSpeechActicityDetection:
+
+    def __init__(self, filename, model=None, segment_out='segments.txt',
+                 segment_dir=None, shout_path=os.environ.get('SHOUT_DIR')):
+        self.filename = filename
+        if model is None:
+            self.model = 'model_name'  # TODO
+        else:
+            self.model
+        self.segment_out = segment_out
+        self.binary_path = '{}/shout_segment'.format(shout_path)
+
+    def segment(self):
+        self.raw_filename = EMSpeechActicityDetection.convert_to_raw(
+            self.filename)
+        cmd = '{0} -a {1} --am-segment {2} -mo {3}'.format(
+            self.binary_path,
+            self.raw_filename,
+            self.model,
+            self.segment_out,
+        )
+        subprocess.call(cmd, shell=True)
+
+    @staticmethod
+    def convert_to_raw(filename):
+        """ accepts mp3, wav and raw files """
+        EMSpeechActicityDetection.__check_audio_file(filename)
+        basename, ext = os.path.splittext(filename)
+        if ext == '.mp3':
+            EMSpeechActicityDetection.convert_mp3_to_wav(filename)
+        fn = EMSpeechActicityDetection.convert_wav_to_raw('{0}.wav'.format(
+            basename))
+        return fn
+
+    @staticmethod
+    def convert_mp3_to_wav(filename):
+        basename, ext = os.path.splittext(filename)
+        out_fn = '{}.wav'.format(basename)
+        subprocess.call('sox {0} {1}'.format(filename, out_fn), shell=True)
+        return out_fn
+
+    @staticmethod
+    def convert_wav_to_raw(filename):
+        basename, ext = os.path.splittext(filename)
+        out_fn = '{}.raw'.format(basename)
+        params = '-r 16k -b 16 -L -c 1'
+        subprocess.call('sox {0} {1} {2}'.format(params, filename, out_fn),
+                        shell=True)
+        return out_fn
+
+    @staticmethod
+    def __check_audio_file(filename):
+        if not os.path.exists(filename):
+            raise Exception('Source file does not exist: {}'.format(
+                filename))
+        ext = os.path.splittext(filename)[-1]
+        if ext not in ('raw', 'mp3', 'wav'):
+            raise ValueError('Cannot handle [{0}] files'.format(
+                ext))
+
+
+def parse_args():
+    p = ArgumentParser()
+    p.add_argument('-i', '--input', type=str,
+                   help='Input file. Use this option if you want to segment'
+                   'a single file'
+                   )
+    p.add_argument('-m', '--model', type=str,
+                   help='SHOUT acoustic model',
+                   default='{}/shout_am.sad'.format(
+                       os.environ.get('SHOUT_DIR')),
+                   )
+    p.add_argument('-o', '--segment-out', type=str,
+                   help='Write segments to file',
+                   default='segments.txt'
+                   )
+    return p.parse_args()
+
+
+def main():
+    args = parse_args()
+    sad = EMSpeechActicityDetection(filename=args.input, model=args.model,
+                                    segment_out=args.segment_out)
+    sad.segment()
+
+if __name__ == '__main__':
+    main()
