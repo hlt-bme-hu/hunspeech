27155cb2c1683283c0edc76a22b9884396c3efb5
Katalin Pajkossy <pajkossy@info.ilab.sztaki.hu>
2016-09-26 14:36:26 +0200

    improved help messages

--- speaker_diarization/em-dia.py
+++ speaker_diarization/em-dia.py
@@ -1,5 +1,5 @@
 import subprocess
-from argparse import ArgumentParser
+from argparse import ArgumentParser, RawTextHelpFormatter
 from os.path import basename, splitext
 import logging
 
@@ -71,20 +71,47 @@ def get_args():
 
     p = ArgumentParser(description='Python wrapper for the speaker diarization module' +\
             ' of the "SHoUT" speech recognition toolkit (by Marijn Huijbregts), with' +\
-            ' added audio conversion (using SoX -Sound eXchange- utility)')
-    p.add_argument('input_fn', help='input audio filename, can be of any' +\
-            ' audio format supported by sox (e.g. .wav, .mp3).' +\
-            ' If the extension is .raw, it is assumed to be a headerless' +\
-            ' raw audio file; mono, 16 kHz, 16 bits little endian.')
-    p.add_argument('output_dir', help='directory where to place all output files' +\
-            ' - the converted raw audio file (.raw), SHOUT outputs (.sad, .dia)' +\
-            ' and a logfile (.log)')
-    p.add_argument('shout_dir', help='directory containing SHOUT source and binaries' +\
-            ' (release-2010-version-0-3)')
-    p.add_argument('-m', '--shout_model', help='statistical model needed for segmenting;' +\
-            ' either this or the output of shout_segment is needed')
-    p.add_argument('-s', '--sad_fn', help='output of shout_segment; either this or a ' +\
-            'statistical model for running shout_segment is needed')
+            ' added audio conversion (using SoX -Sound eXchange- utility)',
+                       formatter_class=RawTextHelpFormatter)
+    p.add_argument('input_fn',
+                   help='''
+                   input audio filename, can be of any audio format 
+                   supported by sox (e.g. .wav, .mp3).
+                   If the extension is .raw, it is assumed to be a
+                   headerless raw audio file; mono, 16 kHz, 16 bits
+                   little endian.
+                  '''
+                  )
+    p.add_argument('output_dir',
+                   help=
+                   '''
+                   directory where to place all output files
+                   - the converted raw audio file (.raw), SHOUT
+                   outputs (.sad, .dia) and a logfile (.log)
+                  '''
+                  )
+    p.add_argument('shout_dir',
+                   help=
+                   '''
+                   directory containing SHOUT source and binaries
+                   (release-2010-version-0-3), available at
+                   http://shout-toolkit.sourceforge.net/download.html
+                  ''')
+    p.add_argument('-m', '--shout_model',
+                   help=
+                   '''
+                   statistical model needed for shout_segment; a
+                   suitable model is available at
+                   http://shout-toolkit.sourceforge.net/download.html
+                   (shout.sad)
+                   if none given, sad_fn argument is expected
+                  ''')
+    p.add_argument('-s', '--sad_fn',
+                   help=
+                   '''
+                   output of shout_segment
+                   if none given, shout_model argument is expected
+                  ''')
     args = p.parse_args()
     return args
 
@@ -101,8 +128,9 @@ def main():
     sad_fn = args.sad_fn
     input_fn = args.input_fn
     if shout_model is None and sad_fn is None:
-        logging.info('Either shout model file or SAD model' +\
-                'file is needed, exiting')
+        logging.info('Either --shout_model (needed for running shout_segment), ' +\
+                     'or --sad_fn (output of shout_segment) argument is needed, ' +\
+                     'see help message for details.')
         quit()
     a = EmDiarizer(shout_dir, shout_model)
     a.process(input_fn, sad_fn, output_dir)
